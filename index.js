9f43b6ec61a5c0ab6b336123db4fd3a92dd7f0cd
Usman Khalid <captainusman133@gmail.com>
2026-08-03 20:52:17 +0500

    Fix Express 5 production catch-all route

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/api/dashboard", dashboardRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
